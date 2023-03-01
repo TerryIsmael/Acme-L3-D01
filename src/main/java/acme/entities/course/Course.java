fde171ed0b405c5688ade6d22eba9911eb6d328e
TerryIsmael <terryismael2@gmail.com>
2023-03-02 00:18:16 +0100

    Course entity and Type enumerate created

---
+++ src/main/java/acme/entities/course/Course.java
@@ -0,0 +1,57 @@
+
+package acme.entities.course;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.entities.enumerates.Type;
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Course extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3} [0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			abstract_;
+
+	@PositiveOrZero
+	protected Double			retailPrice;
+
+	@URL
+	protected String			link;
+	// Derived attributes -----------------------------------------------------
+
+
+	@NotBlank
+	protected Type type() {
+		return null;
+	}
+
+	// Relationships ----------------------------------------------------------
+
+}
