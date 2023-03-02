0e0ffb4077354626adce3211f1159d3ef02d809c
darkgigi <daniel.arriaza.arriaza@gmail.com>
2023-03-02 11:14:06 +0100

    Tutorial entity created

---
+++ src/main/java/entities/tutorials/Tutorial.java
@@ -0,0 +1,53 @@
+
+package entities.tutorials;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Tutorial extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			summary;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			goals;
+
+	// Derived attributes -----------------------------------------------------
+
+
+	protected Date totalTime() {
+		return null;
+	}
+
+	// Relationships ----------------------------------------------------------
+}
