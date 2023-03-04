196392c0a242d6e971fcf30e709dfbf40949e352
josejavier <josalcsan1@alum.us.es>
2023-03-04 12:47:20 +0100

    Fix PracticumSessions

--- src/main/java/entities/PracticumSessions/PracticumSessions.java
+++ src/main/java/entities/PracticumSessions/PracticumSessions.java
@@ -3,6 +3,7 @@ package entities.PracticumSessions;
 
 import java.sql.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -13,6 +14,12 @@ import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractEntity;
 import entities.Practicum.Practicum;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 
 public class PracticumSessions extends AbstractEntity {
 
