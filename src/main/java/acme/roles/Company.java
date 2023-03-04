d62d382e38e24fd92fe06929e6d0b7edd6aa5219
josejavier <josalcsan1@alum.us.es>
2023-03-04 12:44:29 +0100

    Company role entero

---
+++ src/main/java/acme/roles/Company.java
@@ -0,0 +1,33 @@
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Company extends AbstractRole {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			name;
+
+	@NotBlank
+	@Length(max = 26)
+	protected String			VAT;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			summary;
+
+	protected String			link;
+}
