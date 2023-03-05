2f1c41e623b6c02a6f0f54c16cda26fbce7b77f3
TerryIsmael <terryismael2@gmail.com>
2023-03-05 20:45:04 +0100

    Banner entity created

---
+++ src/main/java/acme/entities/banner/Banner.java
@@ -0,0 +1,56 @@
+
+package acme.entities.banner;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Banner extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Past
+	protected Date				instantiation;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date				startDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date				finishDate;
+
+	@URL
+	protected String			pictureLink;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			slogan;
+
+	@URL
+	protected String			documentLink;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
