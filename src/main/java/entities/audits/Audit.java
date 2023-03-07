2fc577a81bea4d3d55de9e1c18086df971dbfc42
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2023-03-07 20:42:44 +0100

    Create Audit entity

---
+++ src/main/java/entities/audits/Audit.java
@@ -0,0 +1,12 @@
+
+package entities.audits;
+
+import acme.framework.data.AbstractEntity;
+
+public class Audit extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long serialVersionUID = 1L;
+
+}
