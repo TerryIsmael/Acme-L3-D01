e67bb70973d1614a40e83faa69b873891ea3dfae
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2023-03-07 20:15:26 +0100

    Create Mark enum

---
+++ src/main/java/entities/audits/Mark.java
@@ -0,0 +1,20 @@
+
+package entities.audits;
+
+public enum Mark {
+
+	A_PLUS("A+"), A("A"), B("B"), C("C"), F("F"), F_MINUS("F-");
+
+
+	private String value;
+
+
+	private Mark(final String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return this.value;
+	}
+}
