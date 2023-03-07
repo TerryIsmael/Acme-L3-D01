bf21a5f256d2b25d18a73da558922a1ad5e242ee
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2023-03-07 21:08:04 +0100

    Create Audit entity

---
+++ src/main/java/acme/entities/audits/Audit.java
@@ -0,0 +1,48 @@
+
+package acme.entities.audits;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.entities.courses.Course;
+import acme.framework.data.AbstractEntity;
+
+public class Audit extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	@Column(unique = true)
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			conclusion;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			strongPoints;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			weakPoints;
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Course			course;
+
+}
