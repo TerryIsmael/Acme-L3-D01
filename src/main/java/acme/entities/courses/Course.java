c7216ef8f8c561c5540ee45f95f5f52187f4915b
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2023-03-07 21:08:35 +0100

    Create Course entity

---
+++ src/main/java/acme/entities/courses/Course.java
@@ -0,0 +1,12 @@
+
+package acme.entities.courses;
+
+import acme.framework.data.AbstractEntity;
+
+public class Course extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long serialVersionUID = 1L;
+
+}
