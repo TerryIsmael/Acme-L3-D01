d4513d9a28caaf9715a0ff42e8108e755da208d2
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2023-03-09 14:02:22 +0100

    Add getter setter to Audit

--- src/main/java/acme/entities/audits/Audit.java
+++ src/main/java/acme/entities/audits/Audit.java
@@ -4,8 +4,12 @@ package acme.entities.audits;
 import javax.persistence.Entity;
 
 import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Audit extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
