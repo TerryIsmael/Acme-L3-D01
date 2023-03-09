2e2ca32781ddcee6178f46e7ffbcb19b5b8b1c1b
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2023-03-09 13:39:39 +0100

    Add anotations on Audit

--- src/main/java/acme/entities/audits/Audit.java
+++ src/main/java/acme/entities/audits/Audit.java
@@ -2,6 +2,7 @@
 package acme.entities.audits;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -12,7 +13,12 @@ import org.hibernate.validator.constraints.Length;
 
 import acme.entities.courses.Course;
 import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Audit extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
