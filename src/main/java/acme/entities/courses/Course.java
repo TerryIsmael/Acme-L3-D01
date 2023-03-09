e9a50a40be96f5749989a88ae3f95a722350e542
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2023-03-09 13:58:13 +0100

    Add getter setter annotation on Course entity

--- src/main/java/acme/entities/courses/Course.java
+++ src/main/java/acme/entities/courses/Course.java
@@ -4,8 +4,12 @@ package acme.entities.courses;
 import javax.persistence.Entity;
 
 import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Course extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
