234a34a2c29c2b71b706727464a03fea3f10d167
TerryIsmael <terryismael2@gmail.com>
2023-03-11 12:43:46 +0100

    Relationship added

--- src/main/java/acme/entities/courses/Course.java
+++ src/main/java/acme/entities/courses/Course.java
@@ -1,9 +1,21 @@
 
 package acme.entities.courses;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.entities.enumerates.CourseType;
+import acme.framework.components.datatypes.Money;
 import acme.framework.data.AbstractEntity;
+import acme.roles.Lecturer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +26,42 @@ public class Course extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long serialVersionUID = 1L;
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3} [0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			abstract_;
+
+	protected Money				retailPrice;
+
+	@URL
+	protected String			link;
+	// Derived attributes -----------------------------------------------------
+
+
+	//TODO: Calcular atributo type
+	@NotBlank
+	protected CourseType type() {
+		return null;
+	}
+
+	// Relationships ----------------------------------------------------------
+
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Lecturer lecturer;
 
 }
