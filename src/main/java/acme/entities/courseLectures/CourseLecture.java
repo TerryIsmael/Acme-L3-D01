24a379fb6486bbe71f53b89aa57db66898c31b3b
TerryIsmael <terryismael2@gmail.com>
2023-03-11 13:14:57 +0100

    Relationship added

---
+++ src/main/java/acme/entities/courseLectures/CourseLecture.java
@@ -0,0 +1,34 @@
+
+package acme.entities.courseLectures;
+
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import acme.entities.courses.Course;
+import acme.entities.lectures.Lecture;
+import acme.framework.data.AbstractEntity;
+
+public class CourseLecture extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Lecture			lecture;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Course			course;
+
+}
