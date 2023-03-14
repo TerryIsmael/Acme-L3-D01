f3f770e794c3f3283c9f5e26e62bf6740ada0e26
virolialo <sowonation.gfriend@gmail.com>
2023-03-14 13:38:28 +0100

    Fix Note

--- src/main/java/acme/entities/note/Note.java
+++ src/main/java/acme/entities/note/Note.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.Valid;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -14,7 +14,6 @@ import javax.validation.constraints.Past;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.framework.components.datatypes.Money;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -32,19 +31,23 @@ public class Note extends AbstractEntity {
 
 	@Temporal(TemporalType.DATE)
 	@Past
+	@NotNull
 	protected Date				instantiationMoment;
 
 	@NotBlank
 	@Length(max = 75)
-	protected String			heading;
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			author;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String			summary;
+	protected String			message;
 
-	@Valid
-	@NotNull
-	protected Money				price;
+	@Email
+	protected String			email;
 
 	@URL
 	protected String			link;
